0a01e69d126a77a4ec6c927863e2a345877a36fb
Slawomir Kussowski <slawomirkussowski@gmail.com>
2021-10-12 18:47:48 +0200

    modified app routing

--- projects/main/src/app/app-routing.module.ts
+++ projects/main/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'projects/auth/src/public-api';
+import { SpotifyComponent } from 'projects/views/src/public-api';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SpotifyComponent,
+    canActivate: [AuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
