cf9a983044942ec4730efe4e976854736236eb25
Slawomir Kussowski <slawomirkussowski@gmail.com>
2021-10-12 18:46:45 +0200

    added views lib

---
+++ projects/views/src/lib/components/spotify/spotify.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-spotify',
+  templateUrl: './spotify.component.html',
+  styleUrls: ['./spotify.component.css']
+})
+export class SpotifyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/views/src/lib/views.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { SpotifyComponent } from './components/spotify/spotify.component';
+
+@NgModule({
+  declarations: [
+    SpotifyComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    SpotifyComponent
+  ]
+})
+export class ViewsModule { }
