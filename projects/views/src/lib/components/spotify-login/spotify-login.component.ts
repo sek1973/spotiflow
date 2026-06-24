5fa302829ec990a33f1bc041704275d9034f3404
Slawomir Kussowski <slawomirkussowskki@gmail.com>
2026-06-24 13:00:21 +0200

    chore: upgrade Angular 21 → 22 (migrate to Vitest)

--- projects/views/src/lib/components/spotify-login/spotify-login.component.ts
+++ projects/views/src/lib/components/spotify-login/spotify-login.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { SpotifyAuthService } from 'projects/auth/src/public-api';
 
 @Component({
     selector: 'lib-spotify-login',
     templateUrl: './spotify-login.component.html',
     styleUrls: ['./spotify-login.component.css'],
+    changeDetection: ChangeDetectionStrategy.Eager,
     standalone: false
 })
 export class SpotifyLoginComponent implements OnInit {
--- projects/views/src/lib/components/spotify/spotify.component.ts
+++ projects/views/src/lib/components/spotify/spotify.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
     selector: 'lib-spotify',
     templateUrl: './spotify.component.html',
     styleUrls: ['./spotify.component.css'],
+    changeDetection: ChangeDetectionStrategy.Eager,
     standalone: false
 })
 export class SpotifyComponent implements OnInit {
